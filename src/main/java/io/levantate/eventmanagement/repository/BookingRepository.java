d769d318c86696613131305fb2dc2cb9179ef515
sai-prasad-1 <sai.prasad.1603@gmail.com>
2024-04-24 22:14:58 +0530

    Update Booking Repo

--- src/main/java/io/levantate/eventmanagement/repository/BookingRepository.java
+++ src/main/java/io/levantate/eventmanagement/repository/BookingRepository.java
@@ -1,6 +1,9 @@
 package io.levantate.eventmanagement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import io.levantate.eventmanagement.models.Booking;
@@ -8,5 +11,5 @@ import io.levantate.eventmanagement.models.Booking;
 
 @Repository 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-   
+  
 }
