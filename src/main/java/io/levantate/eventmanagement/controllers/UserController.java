01f775ddc0c362dd1eb4b610bec8200d3065e26a
sai-prasad-1 <sai.prasad.1603@gmail.com>
2024-04-24 23:13:07 +0530

    Update Status Code

--- src/main/java/io/levantate/eventmanagement/controllers/UserController.java
+++ src/main/java/io/levantate/eventmanagement/controllers/UserController.java
@@ -33,7 +33,7 @@ public ResponseEntity<Object> submitLoginForm(@RequestBody User user) {
     } catch (IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     } catch (Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to login user: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Failed to login user: " + e.getMessage());
     }
 }
 
